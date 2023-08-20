63b2e173dc929118019e9c41d4da47ac9993f38c
sohail <sohailchidre@gmail.com>
2023-08-20 22:34:11 +0530

    conflict resolved between developerA and developerB

--- app.js
+++ app.js
@@ -24,18 +24,16 @@ function mod(a,b)
     return a%b;
 }
 
-<<<<<<< HEAD
 // created in developerA branch 
 
 for(let x of [1,2,3,4,5])
 {
     console.log(x);
-=======
+}
 // created in developerB branch 
 
 let a = [1,2,3,4,5]
 for(let i=0 ;i<a.length ; i++)
 {
     console.log(a[i]);
->>>>>>> developerB
 }
